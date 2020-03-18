05ab0def65517311f02c972130501585a493e632
foolishchow <foolishchow@dingtail.com>
2020-03-18 22:39:00 +0800

    update

--- typeorm/utils/config-parser.d.ts
+++
@@ -1,5 +0,0 @@
-import { EggApplication } from "egg";
-import { ConnectionOptions } from "typeorm";
-import { TypeormConfig } from "../..";
-export declare function parseSynchronizeTypeormConfig(app: EggApplication, config: TypeormConfig): ConnectionOptions[];
-export declare function parseTypeormConfig(app: EggApplication, config: TypeormConfig): ConnectionOptions[];
--- typeorm/utils/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const pure: <T>(data: T) => T;
-export declare function WeakEqual(origin: any, current: any): boolean;
